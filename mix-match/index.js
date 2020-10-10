243b5bacc0725156bd2dfd816b954bccb1fe71aa
blatinegra <54900515+blatinegra@users.noreply.github.com>
2020-10-10 17:46:08 -0400

    edited html and js files

--- mix-match/index.js
+++ mix-match/index.js
@@ -1,3 +1,5 @@
+import { saveAs } from "file-saver";
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
@@ -19,50 +21,68 @@ window.addEventListener("load", init);
 
 shareButton.addEventListener("click", () => {
     console.log("told the truth");
+    canvas.toBlob(function(blob) {
+        saveAs(blob, "myImage.png");
+    })
 });
 
 //bring in images
 let gold1 = new Image();
+gold1.crossOrigin = "anonymous";
 gold1.src = "assets/gold1.png";
 gold1.onload = render;
 let gold2 = new Image();
+gold2.crossOrigin = "anonymous";
 gold2.src = "assets/gold2.png";
 let gold3 = new Image();
+gold3.crossOrigin = "anonymous";
 gold3.src = "assets/gold3.png";
 let gold4 = new Image();
+gold4.crossOrigin = "anonymous";
 gold4.src = "assets/gold4.png";
 let frames = [gold1, gold2, gold3, gold4];
 
 let whitegoldtile = new Image();
+whitegoldtile.crossOrigin = "anonymous";
 whitegoldtile.src = "assets/whitegoldtile.png";
 whitegoldtile.onload = render;
 let mosaictile = new Image();
+mosaictile.crossOrigin = "anonymous";
 mosaictile.src = "assets/mosaictile.png";
 let bluetile = new Image();
 bluetile.src = "assets/bluetile.png";
 let gabstytile = new Image();
+gabstytile.crossOrigin = "anonymous";
 gabstytile.src = "assets/gabstytile.png";
 let tiles = [whitegoldtile, mosaictile, bluetile, gabstytile];
 
 let flower1 = new Image();
+flower1.crossOrigin = "anonymous";
 flower1.src = "assets/flower1.png";
 flower1.onload = render;
 let flower2 = new Image();
+flower2.crossOrigin = "anonymous";
 flower2.src = "assets/flower2.png";
 let flower3 = new Image();
+flower3.crossOrigin = "anonymous";
 flower3.src = "assets/flower3.png";
 let flower4 = new Image();
+flower4.crossOrigin = "anonymous";
 flower4.src = "assets/flower4.png";
 let flowers = [flower1, flower2, flower3, flower4];
 
 let ari = new Image();
+ari.crossOrigin = "anonymous";
 ari.src = "assets/ari.png";
 ari.onload = render;
 let diana = new Image();
+diana.crossOrigin = "anonymous";
 diana.src = "assets/diana.png";
 let megan = new Image();
+megan.crossOrigin = "anonymous";
 megan.src = "assets/megan.png";
 let chaka = new Image();
+chaka.crossOrigin = "anonymous";
 chaka.src = "assets/chaka.png";
 let blackWomenIs = [megan, ari, chaka, diana];
 
