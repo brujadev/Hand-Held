70d06f6d353f0c384f29eaf3b935d6e513e89db7
blatinegra <54900515+blatinegra@users.noreply.github.com>
2020-10-17 22:02:01 -0400

    added touch events to js

--- pentool/main.js
+++ pentool/main.js
@@ -29,6 +29,27 @@ canvas.addEventListener("mousedown", function(evt) {
     ctx.stroke();
 });
 
+canvas.addEventListener("touchstart", function(evt) {
+    let touches = Array.from(evt.touches);
+    let touch = touches[0];
+
+    penDown = true;
+
+    let x = touch.clientX;
+    let y = touch.clientY;
+
+    last_x = x;
+    last_y = y;
+
+    ctx.beginPath();
+    // ctx.moveTo(0, 0);
+    // ctx.arc(x, y, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fillRect(x - 5, y - 5, 10, 10);
+    // ctx.lineTo(x, y);
+    ctx.stroke();
+});
+
 canvas.addEventListener("mousemove", function(evt) {
     if (penDown == false) {
         return;
@@ -44,6 +65,25 @@ canvas.addEventListener("mousemove", function(evt) {
     // ctx.fillStyle = "red";
     // ctx.fillRect(x - 5, y - 5, 10, 10);
     // ctx.lineTo(x, y);
+    ctx.stroke();
+
+    last_x = x;
+    last_y = y;
+});
+
+canvas.addEventListener("touchmove", function(evt) {
+    evt.preventDefault();
+
+    let touches = Array.from(evt.touches);
+    let touch = touches[0];
+
+    let x = touch.clientX;
+    let y = touch.clientY;
+
+    ctx.beginPath();
+    ctx.moveTo(last_x, last_y);
+    ctx.lineTo(x, y);
+
     ctx.stroke();
 
     last_x = x;
